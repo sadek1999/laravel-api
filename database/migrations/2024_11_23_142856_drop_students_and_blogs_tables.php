ba114d9d0164c0313310b3d4c11e0663a3d8cccc
sadek1999 <sadekali1530@gmail.com>
2024-11-23 20:33:37 +0600

    successfully delete the blogs and students table

--- database/migrations/2024_11_23_142856_drop_students_and_blogs_tables.php
+++ database/migrations/2024_11_23_142856_drop_students_and_blogs_tables.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blogs', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('students', function (Blueprint $table) {
+            Schema::dropIfExists('students');
+            Schema::dropIfExists('blogs');
         });
     }
 
@@ -22,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blogs');
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\StudentController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,8 +8,4 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/students', [StudentController::class, 'index'])->name('student.index');
-Route::post('/students', [StudentController::class, 'store'])->name('student.store');
-Route::get('/students/{id}', [StudentController::class, 'show'])->name('student.show');
-Route::put('/students/{id}', [StudentController::class, 'update'])->name('student.update');
-Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+
